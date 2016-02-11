7ca83411cac4555300bde6c8719bc055f525e614
Aenerb <aenerb@gmail.com>
2016-02-10 21:07:55 -0600

    Updated weightsMinor to have 9 indexes (makes things easier later)

--- src/ItemLookupTables.java
+++ src/ItemLookupTables.java
@@ -6,7 +6,7 @@ public class ItemLookupTables {
     /**
      * The following tables match with the random item generation tables found in the Core Rulebook
      */
-    private static int[] weightsMinor = new int[]{4,5,35,2,35,10,9};
+    private static int[] weightsMinor = new int[]{4,5,35,2,0,35,0,10,9};
     private static int[] weightsMedium = new int[]{10,10,10,10,10,15,3,15,17};
     private static int[] weightsMajor = new int[]{10,10,5,10,10,10,20,5,20};
     private static int[] armorMinor = new int[]{60,20,5,2,2,2,9};
